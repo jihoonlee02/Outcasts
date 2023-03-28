cddf892deaf28247cb479f082d11cceb2a597689
Ryan A <ryanabusa8134@gmail.com>
2023-03-28 15:21:23 -0400

    Projectile Edits

--- Assets/Scripts/Tools/Projectile.cs
+++ Assets/Scripts/Tools/Projectile.cs
@@ -7,24 +7,29 @@ public class Projectile : MonoBehaviour
     private Vector2 m_movingDirection;
 
     [SerializeField] private float m_speed = 1.5f;
-    [SerializeField] private SpriteRenderer m_sr;
-    [SerializeField] private Rigidbody2D m_rb;
-    private Transform priorParent;
+    private SpriteRenderer m_sr;
+    private Rigidbody2D m_rb;
+    private Collider2D m_cldr;
+    private Transform poolerParent;
     public void Awake()
     {
         m_movingDirection = Vector2.zero;
         m_sr = GetComponent<SpriteRenderer>();
         m_rb = GetComponent<Rigidbody2D>();
+        m_cldr = GetComponent<Collider2D>();
+        poolerParent = transform.parent;
     }
 
     public void Fire(Vector2 spawnPos, Vector2 direction)
     {
-        transform.SetParent(priorParent, true);
+        transform.SetParent(poolerParent, true);
         transform.position = spawnPos;
         m_movingDirection = direction;
-        enabled = true;
+        m_rb.velocity = direction * 10f;
+        //enabled = true;
 
         m_sr.flipX = direction == Vector2.left;
+        m_cldr.offset = new Vector2(Mathf.Sign(direction.x) * Mathf.Abs(m_cldr.offset.x), m_cldr.offset.y);
         if (direction == new Vector2(0.5f, 0.5f))
         {
             transform.eulerAngles = new Vector3(0f, 0f, 45);
@@ -50,7 +55,7 @@ public class Projectile : MonoBehaviour
 
     public void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + m_movingDirection, Time.deltaTime * m_speed);
+        //transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + m_movingDirection, Time.deltaTime * m_speed);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -58,9 +63,9 @@ public class Projectile : MonoBehaviour
         //Debug.Log("I was entered trigger!");
         if (collider.gameObject.layer == LayerMask.NameToLayer("Platforms"))
         {
-            priorParent = transform.parent;
             m_rb.velocity = Vector3.zero;
-            enabled = false;
+            //m_movingDirection = Vector3.zero;
+            //enabled = false;
             transform.SetParent(collider.transform, true);
         }
     }
@@ -70,9 +75,9 @@ public class Projectile : MonoBehaviour
         //Debug.Log("I was entered Collision!");
         if (collision.gameObject.layer == LayerMask.NameToLayer("Platforms"))
         {
-            priorParent = transform.parent;
             m_rb.velocity = Vector3.zero;
-            enabled = false;
+            //m_movingDirection = Vector3.zero;
+            //enabled = false;
             transform.SetParent(collision.transform, true);
         }
     }
