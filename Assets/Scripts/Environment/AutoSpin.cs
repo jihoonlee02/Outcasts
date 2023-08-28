9315cb0f12365fcd19f755fbf3bb83228fdb671f
Ryan Abusaada <71282474+ryanabusa8134@users.noreply.github.com>
2023-08-27 21:29:46 -0400

    More level

--- Assets/Scripts/Environment/AutoSpin.cs
+++ Assets/Scripts/Environment/AutoSpin.cs
@@ -7,6 +7,6 @@ public class AutoSpin : MonoBehaviour
     [SerializeField] private float m_speed = 2f;
     private void FixedUpdate()
     {
-        transform.Rotate(0f, 0f, m_speed * Time.deltaTime, Space.World);
+        transform.Rotate(0f, 0f, m_speed * Time.deltaTime, Space.Self);
     }
 }
