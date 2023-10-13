bb2d3706178038367900a844487b3284c55f2df4
Ryan Abusaada <71282474+ryanabusa8134@users.noreply.github.com>
2023-10-12 23:25:20 -0400

    parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    [SerializeField] private GameObject m_camera;
+    [SerializeField] private float parallaxEffect;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (m_camera.transform.position.x * (1 - parallaxEffect));
+        float dist = (m_camera.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
