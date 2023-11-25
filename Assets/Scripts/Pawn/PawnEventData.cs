f6feb778540bd30f963e9b522c1170aa303de628
Ryan Abusaada <71282474+ryanabusa8134@users.noreply.github.com>
2023-11-25 11:26:35 -0500

    Pawn Event Data UI Glowup

--- Assets/Scripts/Pawn/PawnEventData.cs
+++ Assets/Scripts/Pawn/PawnEventData.cs
@@ -20,6 +20,7 @@ public struct PawnEvent
     // TODO: Be able to adjust both Tinker and Actions Actions in the Same Event
     // Include Dialogue to be ran in Dialogue Manager!
     [Header("Level General")] // More Efficent in a new editor
+    [SerializeField] private string pawnEventTitle;
     [SerializeField] private bool pausePawnControl;
     [SerializeField] private bool resumePawnControl;
     [Header("Pawn Specific")]
@@ -85,52 +86,116 @@ public enum Direction
 [CustomEditor(typeof(PawnEventData)), CanEditMultipleObjects]
 public class PawnEventDataEditor : Editor
 {
-    //bool pawnEventFoldOut;
-    //public override void OnInspectorGUI()
-    //{
-    //    base.OnInspectorGUI();
-    //    PawnEventData pawnEventData = (PawnEventData)target;
-    //    EditorGUILayout.Space();
-    //    pawnEventFoldOut = EditorGUILayout.Foldout(pawnEventFoldOut, "PawnEvents");
-    //    foreach (PawnEvent p in pawnEventData.PawnEvents)
-    //    {
-    //        EditorGUILayout.BeginHorizontal();
-    //        switch (p.EventAction)
-    //        {
-    //            case EventAction.Move:
-    //                EditorGUILayout.LabelField("Moving");
-    //                break;
-    //            case EventAction.Jump:
-    //                EditorGUILayout.LabelField("Jumping");
-    //                break;
-    //            case EventAction.Punch:
-    //                EditorGUILayout.LabelField("Punching");
-    //                break;
-    //            case EventAction.Shoot:
-    //                EditorGUILayout.LabelField("Shooting");
-    //                break;
-    //            case EventAction.Grab:
-    //                EditorGUILayout.LabelField("Grabbing");
-    //                break;
-    //        }
-    //        EditorGUILayout.EndHorizontal();
-    //        EditorGUILayout.Space();
-    //    }
-    //}
+    SerializedProperty pawnEventArray;
+    int currPawnEventIdx;
+    private bool areYouSure = false;
+    private void OnEnable()
+    {
+        pawnEventArray = serializedObject.FindProperty("pawnEvents");
+    }
 
-    //public override VisualElement CreateInspectorGUI()
-    //{
-    //    // Create a new VisualElement to be the root of out inspector UI
-    //    VisualElement myInspector = new VisualElement();
+    public override void OnInspectorGUI()
+    {
+        // base.OnInspectorGUI();
+        serializedObject.Update();
+        EditorGUILayout.LabelField("Pawn Event Data");
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField("Events: " + pawnEventArray.arraySize, EditorStyles.boldLabel);
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("+ New Pawn Event To End", GUILayout.MaxHeight(20f)))
+        {
+            currPawnEventIdx = pawnEventArray.arraySize++;
+            SerializedProperty newElementProperty = pawnEventArray.GetArrayElementAtIndex(pawnEventArray.arraySize - 1);
+            InitializePawnEvent(newElementProperty);
+        }
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("+ New Pawn Event at " + currPawnEventIdx, GUILayout.MaxHeight(20f)))
+        {
+            pawnEventArray.InsertArrayElementAtIndex(currPawnEventIdx);
+            SerializedProperty newElementProperty = pawnEventArray.GetArrayElementAtIndex(currPawnEventIdx);
+            InitializePawnEvent(newElementProperty);
+        }
+        GUILayout.FlexibleSpace();
+        if (!areYouSure)
+        {
+            areYouSure = GUILayout.Button("- Delete Pawn Event " + currPawnEventIdx, GUILayout.MaxHeight(20f));
+        }
+        else 
+        {
+            EditorGUILayout.LabelField("Are you sure? ", GUILayout.MaxWidth(80f));
+            if (GUILayout.Button("Yes", GUILayout.MaxHeight(20f), GUILayout.ExpandWidth(true)))
+            {
+                pawnEventArray.DeleteArrayElementAtIndex(currPawnEventIdx);
+                currPawnEventIdx--;
+                areYouSure = false;
+            }
+            else if (GUILayout.Button("No", GUILayout.MaxHeight(20f), GUILayout.ExpandWidth(true)))
+            {
+                areYouSure = false;
+            }
+        }
+        
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
 
-    //    // Add a simple label
-    //    myInspector.Add(new Label("PawnEvents Internal Title"));
+        GUILayout.FlexibleSpace();
 
-    //    // Load and clone a visual tree from UXML
-    //    VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/VisualUITrees/PawnEventsCustomInspector.uxml");
-    //    visualTree.CloneTree(myInspector);
+        if (GUILayout.Button("<<", EditorStyles.miniButtonLeft, GUILayout.MaxWidth(50f)) && currPawnEventIdx > 0) currPawnEventIdx--;
 
-    //    return myInspector;
-    //}
+        GUILayout.Space(20);
+
+        EditorGUILayout.IntField(currPawnEventIdx, GUILayout.MaxWidth(50f));
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button(">>", EditorStyles.miniButtonRight, GUILayout.MaxWidth(50f)) && currPawnEventIdx < pawnEventArray.arraySize - 1) currPawnEventIdx++;
+
+        // Bound the index
+        currPawnEventIdx = pawnEventArray.arraySize > 0 
+            ? (currPawnEventIdx % pawnEventArray.arraySize + pawnEventArray.arraySize) % pawnEventArray.arraySize : 0;
+
+        GUILayout.FlexibleSpace();
+
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+
+        // (Ryan) From here is where you will add your custom implementation of Pawn Events
+        SerializedProperty selectedElement = pawnEventArray.GetArrayElementAtIndex(currPawnEventIdx);
+        selectedElement.isExpanded = true;
+        EditorGUILayout.PropertyField(selectedElement, true);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    // So that we can just have our struct initializes with wanted defaults
+    private void InitializePawnEvent(SerializedProperty pawnEventProperty)
+    {
+        // Level General
+        pawnEventProperty.FindPropertyRelative("pawnEventTitle").stringValue = "New Event " + pawnEventArray.arraySize;
+        pawnEventProperty.FindPropertyRelative("pausePawnControl").boolValue = false;
+        pawnEventProperty.FindPropertyRelative("resumePawnControl").boolValue = false;
+        // Pawn Specific
+        pawnEventProperty.FindPropertyRelative("pawnSelection").enumValueIndex = 0;
+        pawnEventProperty.FindPropertyRelative("eventAction").enumValueIndex = 0;
+        // Movement Event
+        pawnEventProperty.FindPropertyRelative("timeDuration").floatValue = 0;
+        pawnEventProperty.FindPropertyRelative("moveDirection").enumValueIndex = 0;
+        pawnEventProperty.FindPropertyRelative("moveSpeed").floatValue = 0.2f;
+        pawnEventProperty.FindPropertyRelative("delay").floatValue = 0;
+        // Jump Event
+        pawnEventProperty.FindPropertyRelative("jumpForce").floatValue = 0;
+        // Dialogue
+        pawnEventProperty.FindPropertyRelative("activeDialogueAtTime").boolValue = false;
+        pawnEventProperty.FindPropertyRelative("waitOnDialogue").boolValue = false;
+        pawnEventProperty.FindPropertyRelative("dialogues").arraySize = 0;
+        // Invoke Event
+        pawnEventProperty.FindPropertyRelative("invoke").boolValue = false;
+        pawnEventProperty.FindPropertyRelative("activate").boolValue = false;
+        pawnEventProperty.FindPropertyRelative("id").intValue = -1;
+    }
 }
 #endif
